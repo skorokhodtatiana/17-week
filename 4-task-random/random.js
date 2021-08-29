1c129ff96e17e3316dbd41c5bc50dfe8521ac5fa
skorokhodtatiana <tanjakharza@mail.ru>
2021-08-29 13:35:21 +0300

    finished this task

---
+++ 4-task-random/random.js
@@ -0,0 +1,53 @@
+const startRandom = document.getElementById('startRandom');
+const resultRandom = document.getElementById('resultRandom');
+
+function startScript() {
+    let randomNumberArr = createRandomNumber();
+    setRandomNumber(randomNumberArr);
+    setMaxNumber(randomNumberArr);
+    setMinNumber(randomNumberArr);
+    setSumNumber(randomNumberArr);
+    setAverage(randomNumberArr);
+    multiplicationEachNumberArr(randomNumberArr);
+}
+
+function createRandomNumber() {
+    let listRandomNumber = [];
+    for (i = 0; i < 10; i++) {
+        let randomNumber = Math.ceil(Math.random() * 10) * (Math.round(Math.random()) ? 1 : -1);
+        let arrListRandomNumber = listRandomNumber.push(randomNumber);
+    }
+    return listRandomNumber;
+}
+
+function setRandomNumber(listRandomNumber) {
+    resultRandom.innerHTML = "Сгенерировали " + listRandomNumber + "<br/>";
+}
+
+function setMaxNumber(listRandomNumber) {
+    let maxNumber = Math.max.apply(null, listRandomNumber);
+    resultRandom.innerHTML += "Максимальное число равно " + maxNumber.toString() + "<br/>";
+}
+
+function setMinNumber(listRandomNumber) {
+    let minNumber = Math.min.apply(null, listRandomNumber);
+    resultRandom.innerHTML += "Минимальное число равно " + minNumber.toString() + "<br/>";
+}
+
+function setSumNumber(listRandomNumber) {
+    let sumNumberResult = listRandomNumber.reduce((a, b) => a + b);
+    resultRandom.innerHTML += "Сумма чисел равна " + sumNumberResult + "<br/>";
+}
+
+function setAverage(listRandomNumber) {
+    let resultAverage = listRandomNumber.reduce((a, b) => a + b) / listRandomNumber.length;
+    resultRandom.innerHTML += "Среднеарифметическое равно " + resultAverage + "<br/>";
+}
+
+function multiplicationEachNumberArr(listRandomNumber) {
+    let resultMultiplication = listRandomNumber.reduce((a, b) => a * b);
+    resultRandom.innerHTML += "Произведение чисел " + resultMultiplication + "<br/>";
+}
+
+
+startRandom.addEventListener('click', startScript);
